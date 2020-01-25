b60a312abb6a56118d07e53b109b190d3c033bcd
KJFreys1 <kfreyermuth20@gmail.com>
2020-01-25 15:42:41 -0800

    moved index file for server to root directory for deploy

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const parser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(parser.urlencoded({ extended: true} ))
 app.use(parser.json())
 
-const ideaController = require('./controllers/Idea')
+const ideaController = require('./backend/controllers/Idea')
 
 app.use('/ideas', ideaController)
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
